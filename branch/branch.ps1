6270a736b59ebb5512c2ea7fee482f8581e60ac9
danjhl <danjhl.dev@gmail.com>
2023-12-23 20:56:50 +0100

    Pass options to git branch

--- branch/branch.ps1
+++ branch/branch.ps1
@@ -1,8 +1,19 @@
+
 # Options
 $merge = "merge selected into current"
 $rebase = "rebase current onto selected"
 
-$br = $(git branch -r --format='%(refname:short)' | fzf)
+$a = "-r"
+if ($args) {
+    $a = $args
+}
+
+$br = $(git branch $a --format='%(refname:short)' | fzf)
+
+if (!$br) {
+    exit 1
+}
+
 $op = $("$merge`n$rebase" | fzf)
 
 if ($op -eq $merge) {
