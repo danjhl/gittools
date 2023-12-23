eb5d55f09f4bb4871a984104370dd236c6a158c0
danjhl <danjhl.dev@gmail.com>
2023-12-23 20:04:28 +0100

    Fix no selection issue on esc

--- switch-branch/sb.ps1
+++ switch-branch/sb.ps1
@@ -2,4 +2,6 @@ $branches = $(git branch -a --format='%(refname:short)')
 $branches = $branches -replace "origin\/", ""
 $selected = $branches | sort | get-unique | fzf
 
-git switch $selected
+if ($selected) {
+	git switch $selected
+}
